cf18c22cbe624a6019072a5ddb98667c1ed508a0
Max Borghino <fmborghino@gmail.com>
2024-12-14 11:16:00 -0800

    Day13, part2 add offset and switch to Long

--- src/Day13.kt
+++ src/Day13.kt
@@ -41,29 +41,26 @@ fun main() {
         }
     }
 
-    fun cramer(machine: Machine): Int {
+    fun cramer(machine: Machine, offset: Long): Long {
         with(machine) {
+            val px = p.x + offset
+            val py = p.y + offset
             val d = (a.x * b.y - a.y * b.x)
-            val pressA = (p.x * b.y - p.y * b.x) / d
-            val pressB = (a.x * p.y - a.y * p.x) / d
+            val pressA = (px * b.y - py * b.x) / d
+            val pressB = (a.x * py - a.y * px) / d
 
             val dX = pressA * a.x + pressB * b.x
             val dY = pressA * a.y + pressB * b.y
 
-            return if (dX == p.x && dY == p.y) 3 * pressA + pressB else 0
+            return if (dX == px && dY == py) 3 * pressA + pressB else 0
         }
     }
 
-    fun part1(input: List<String>): Int {
-        return parse(input).sumOf { cramer(it) }
-    }
-
-    fun part2(input: List<String>): Int {
-        return 2 * input.sumOf { it.toInt() }
+    fun part1(input: List<String>, offset: Long = 0): Long {
+        return parse(input).sumOf { cramer(it, offset) }
     }
 
     verify("Test part 1", part1(readInput("Day${day}_test")), 480)
     verify("Real part 1", part1(readInput("Day${day}")), 39996)
-//    verify("Test part 2", part2(readInput("Day${day}_test")), 2)
-//    verify("Real part 2", part2(readInput("Day${day}")), 20)
+    verify("Real part 2", part1(readInput("Day${day}"), offset=10_000_000_000_000L), 73267584326867)
 }
